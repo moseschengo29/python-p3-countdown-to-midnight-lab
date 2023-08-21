c4d8bf86dd8eb8865521b568103e05aa41a6d574
Moses <mochengo29@gmail.com>
2023-08-21 21:29:30 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+
+def countdown(count): 
+    while count > 0:
+        print(f'{count} SECOND(S)!')
+        
+        count -= 1
+        
+    print('HAPPY NEW YEAR!')
+countdown(5)            
